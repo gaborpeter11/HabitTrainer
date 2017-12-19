7df433163e055305aa6d7ec189cafd84cb9b90b8
peter.gabor <gaborpeter11@gmail.com>
2017-12-19 15:30:31 +0100

    create layout

--- app/src/main/java/com/example/petergabor/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/petergabor/habittrainer/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.petergabor.habittrainer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = getString(R.string.drink_water)
+        tv_description.text = "A refreshin glass of water gets you hydrated"
+
+
     }
 
-    // hello from kotlin
 
 }
