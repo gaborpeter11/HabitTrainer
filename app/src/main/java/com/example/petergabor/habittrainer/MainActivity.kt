335656f152335dbbf648ee8429bd8aca7f8e4854
peter.gabor <gaborpeter11@gmail.com>
2017-12-19 13:01:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/petergabor/habittrainer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petergabor.habittrainer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
